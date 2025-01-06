54ef550b2045fbe2a0e1b2f4a55cc91bcaae6a73
sagarpurkuti <020bim038@sxc.edu.np>
2025-01-06 21:16:17 +0545

    fixed logout

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, CustomLogoutView
 from django.contrib.auth.views import LogoutView
 
 
@@ -7,7 +7,9 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns=[
     path('login/', CustomLoginView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    # path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    path('logout/', CustomLogoutView.as_view(), name="logout"),
+
     path('', TaskList.as_view(), name="tasklist"),
     path('task/<int:pk>/', TaskDetail.as_view(), name="task"),
     path('task-edit/<int:pk>/', TaskUpdate.as_view(), name="task-update"),
